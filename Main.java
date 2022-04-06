8965c7a91e24f5aab35544c788666b25345b7f4d
bangmaple <bangmapleproject@gmail.com>
2022-04-06 20:24:03 +0700

    Changed Main class to another message

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello bang");
+        System.out.println("hello 1234567890");
+        System.out.println("123456786543245");
     }
 }
